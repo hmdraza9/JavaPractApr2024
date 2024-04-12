ecf08489145203eb4d9264cfbad2b00d6ef22ada
Hamid Raza <hamid_raza@epam.com>
2024-04-12 20:38:27 +0530

    Added Java file rename code

---
+++ src/main/java/JavaPractice/JavaPractApr2024/App.java
@@ -0,0 +1,13 @@
+package JavaPractice.JavaPractApr2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
